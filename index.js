3c34772f0d30b5120027a755ea93522800c58c1a
William Tong <william120110@gmail.com>
2026-03-07 17:16:03 +1300

    static file handler

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+
+const app = express();
+
+app.use(express.static('public'))
+
+app.get('/chat', (req, res) => {
+    res.send('welcome')
+})
+
+
+app.get('/wow', (req, res) => {
+    res.send('welcome')
+})
+
+
+app.listen(3000,()=> {
+    console.log('Server running at http://localhost:3000')
+})
